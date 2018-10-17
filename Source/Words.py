aa0f3af657fc0a302314fd0169b0ec9ed75a5295
codebleu13 <44237226+codebleu13@users.noreply.github.com>
2018-10-17 12:40:12 -0400

    Update Words.py

--- Source/Words.py
+++ Source/Words.py
@@ -1,57 +1,28 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def loadFile():\n",
-    "    isFile = False\n",
-    "    while (isFile == False):\n",
-    "        newfile = input(\"Enter a file please:\")\n",
-    "        try:\n",
-    "            workFile = open(newfile)\n",
-    "            isFile = True\n",
-    "            return workFile\n",
-    "        except:\n",
-    "            isFile = False\n",
-    "            print(\"THAT IS NOT A FILE. PLEASE TRY AGAIN\")\n",
-    "\n",
-    "workFile = loadFile()\n",
-    "allWords = []\n",
-    "for line in workFile:\n",
-    "    line = line.lower()\n",
-    "    line = line.split('\\n')[0]\n",
-    "    lineCheck = line.split(' ')\n",
-    "    for word in lineCheck:\n",
-    "        if word not in allWords:\n",
-    "            allWords.append(word)\n",
-    "            \n",
-    "allWords.sort()\n",
-    "print(allWords)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3.7",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+isFile = False
+while (isFile == False):
+    newfile = input("Enter a file please:")
+    try:
+        workFile = open(newfile)
+        isFile = True
+    except:
+        isFile = False
+        print("THAT IS NOT A FILE. PLEASE TRY AGAIN")
+
+count = 0
+spamSum = 0
+hiCount = 0
+for line in workFile:        
+    spam = line
+
+    perc = spam.split(' ', 1)[1]
+    perc = float(perc)
+    spamSum += perc
+    count = count + 1
+    if perc > .90:
+        hiCount = hiCount + 1
+
+totPerc = spamSum/count
+        
+
+print("The average spam confidece is:" , totPerc)
+print("The number of emails above 90% spam is:" , hiCount)
