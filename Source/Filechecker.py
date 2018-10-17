8133dd73f1e4765ddcd2c841e7bfd6a662c1636c
codebleu13 <44237226+codebleu13@users.noreply.github.com>
2018-10-17 12:38:39 -0400

    Update Filechecker.py

--- Source/Filechecker.py
+++ Source/Filechecker.py
@@ -1,48 +1,12 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def loadFile():\n",
-    "    isFile = False\n",
-    "    while (isFile == False):\n",
-    "        newfile = input(\"Enter a file please:\")\n",
-    "        try:\n",
-    "            workFile = open(newfile)\n",
-    "            isFile = True\n",
-    "            for line in workFile:\n",
-    "                print(line)\n",
-    "            #return workFile\n",
-    "        except:\n",
-    "            isFile = False\n",
-    "            print(\"THAT IS NOT A FILE. PLEASE TRY AGAIN\")\n",
-    "\n",
-    "loadFile()\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3.7",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+def loadFile():
+    isFile = False
+    while (isFile == False):
+        newfile = input("Enter a file please:")
+        try:
+            workFile = open(newfile)
+            isFile = True
+        except:
+            isFile = False
+            print("THAT IS NOT A FILE. PLEASE TRY AGAIN")
+
+loadFile()
